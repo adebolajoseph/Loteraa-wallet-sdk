678d845fb767f15b324ce8c88fba764236a036fa
adebolajoseph <153661201+adebolajoseph@users.noreply.github.com>
2025-10-03 17:17:43 +0100

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import React from 'react'
+import { ThemeProvider as Provider } from 'next-themes'
+import type { ThemeProviderProps as ProviderOptions } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function UIThemeProvider({ children, ...options }: ProviderOptions) {
+  return <Provider {...options}>{children}</Provider>
 }
